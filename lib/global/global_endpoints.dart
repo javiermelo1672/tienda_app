6dbe44ed96b8c0e1f5c7be417049f2637b88f0a3
Javier Melo <javier.hospital@bodytechcorp.com>
2021-12-02 20:51:20 -0500

    adding orders enpoints

--- lib/global/global_endpoints.dart
+++ lib/global/global_endpoints.dart
@@ -1,3 +1,5 @@
 final String globalEndpoint = "http://localhost:8080/";
 final String products = "productos";
 final String agregar = "agregar";
+final String guardarOrden = "apimid/guardarPedido";
+final String getOrdenes = "apimid/consultarPedidos/2";
